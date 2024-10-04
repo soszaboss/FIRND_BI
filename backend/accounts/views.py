af73be05fa86427b4e1c26dc5205085ba039e9cb
soszaboss <moustoifa10@outlook.fr>
2024-10-04 19:13:52 +0000

    modification de la création de compte au niveau de institution

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -24,7 +24,7 @@ class DiplomeUserList(generics.ListCreateAPIView):
 class InstitutionUserList(generics.ListCreateAPIView):
 
     queryset = Institution.objects.all()
-    serializer_class = DiplomeUserSerializer
+    serializer_class = InstitutionUserSerializer
 
     def list(self, request):
         queryset = self.get_queryset()
