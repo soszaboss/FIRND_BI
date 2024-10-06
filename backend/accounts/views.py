5c84246c4bd398aace4301fe72329ae1fd722318
soszaboss <moustoifa10@outlook.fr>
2024-10-06 00:50:37 +0000

    mise à jour du readme.md

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -34,7 +34,7 @@ class InstitutionUserList(generics.ListCreateAPIView):
 class AdminUserList(generics.ListCreateAPIView):
 
     queryset = Admin.objects.all()
-    serializer_class = DiplomeUserSerializer
+    serializer_class = AdminUserSerializer
 
     def list(self, request):
         queryset = self.get_queryset()
